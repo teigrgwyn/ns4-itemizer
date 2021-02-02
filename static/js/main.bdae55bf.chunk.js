8cea79c55b8f4a756b54f22b0f8ca519def2f2f9
Cory Young <teigrgwyn@gmail.com>
2021-02-01 18:58:24 -0600

    Updates

---
+++ static/js/main.bdae55bf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpns4-itemizer"]=this["webpackJsonpns4-itemizer"]||[]).push([[0],{10:function(e,n,t){},11:function(e,n,t){"use strict";t.r(n);var c=t(0),i=t(1),r=t.n(i),s=t(3),o=t.n(s);t(9),t(10);var a=function(){return Object(c.jsx)("div",{className:"App",children:"placeholder"})};o.a.render(Object(c.jsx)(r.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.bdae55bf.chunk.js.map
