b0ed3cecd391a984fd11299c45b114cdb83b69fa
Cory Young <teigrgwyn@gmail.com>
2021-02-04 15:18:14 -0600

    added placeholder images for gear

--- src/App.js
+++ src/App.js
@@ -25,6 +25,246 @@ const initialCards = [
     keen: true,
     phys: '2d12 Pierce',
     ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Crafted Enchanted Two-bladed Sword',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [1]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [2]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [2]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [3]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [3]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [4]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [4]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [5]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [5]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [6]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [6]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [7]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [7]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [8]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [8]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [9]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [9]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  },
+  {
+    // required
+    name: 'Justice [10]',
+    level: '20',
+    type: 'Weapon',
+
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade [10]',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
   }
 ]
 
@@ -43,6 +283,7 @@ export default function App() {
     setQuery({ ...query, [event.target.name]: event.target.value });
   }
 
+  // recompiles cardstack every render
   filteredCards = initialCards.filter(card => card.name.toLowerCase().includes(query.text.toLowerCase()));
 
   return (
@@ -65,7 +306,7 @@ export default function App() {
         {
           filteredCards.map(card => (
             <div className='card' key={card.name}>
-              <h3>Item: {card.name}</h3>
+              <h3>{card.name}</h3>
               <h4>Level: {card.level}</h4>
               <h4>Base: {card.base}</h4>
               <h4>Enhancement: {card.enhancement}</h4>
