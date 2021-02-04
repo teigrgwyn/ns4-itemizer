0420b9ac7448ac7281c25cd6604c83530784ad27
Cory Young <teigrgwyn@gmail.com>
2021-02-03 19:57:50 -0600

    enabled basic search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 // dependencies
 import { useState } from 'react';
 
-// components
-import Search from './components/Search';
-// import Cards from './components/Cards';
+const initialCards = [
+  {
+    // required
+    name: 'Justice',
+    level: '20',
+    type: 'Weapon',
 
-// styles
-import './App.css';
+    // optional,
+    base: 'Light Hammer',
+    enhancement: 4,
+    keen: true,
+    phys: '2d8 Bludge',
+    ele: '1d8 Posi'
+  },
+  {
+    name: 'Voidmind Blade',
+    level: '35',
+    type: 'Weapon',
+
+    base: 'Shortsword',
+    enhancement: 7,
+    keen: true,
+    phys: '2d12 Pierce',
+    ele: '1d8 Acid, 1d4 Nega'
+  }
+]
+
+let filteredCards = [];
 
 const initialQuery = {
   text: ''
@@ -14,17 +36,47 @@ const initialQuery = {
 
 export default function App() {
   const [query, setQuery] = useState(initialQuery);
-  // const [displayCards, setDisplayCards] = useState([]);
+  // const [displayCards] = useState(initialCards);
   // const [savedCards, setSavedCards] = useState([]);
 
   const onQueryChange = event => {
     setQuery({ ...query, [event.target.name]: event.target.value });
   }
 
+  filteredCards = initialCards.filter(card => !card.name.toLowerCase().indexOf(query.text.toLowerCase()));
+
   return (
     <div id="app">
-      <Search query={query} onQueryChange={onQueryChange} />
-      {/*<Cards displayCards={displayCards} setDisplayCards={onSetDisplayCards()} />*/}
+      {/* ========================= */}
+      <div id="query">
+        <input
+          name='text'
+          value={query.text}
+          onChange={onQueryChange}
+          placeholder='search'
+          spellCheck='false'
+          autoComplete='off'
+        />
+      </div>
+      {/* ========================= */}
+      <div id="padding" />
+      <div id="cards">
+        {
+          filteredCards.map(card => (
+            <div className='card' key={card.name}>
+              <h3>Item: {card.name}</h3>
+              <h4>Level: {card.level}</h4>
+              <h4>Base: {card.base}</h4>
+              <h4>Enhancement: {card.enhancement}</h4>
+              <h4>Keen: {card.keen ? 'True' : 'False'}</h4>
+              <h4>Damage:</h4>
+              <h4>~{card.phys}</h4>
+              <h4>~{card.ele}</h4>
+            </div>
+          ))
+        }
+      </div>
+      {/* ========================= */} 
     </div> 
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 // components
 import App from './App';
 
+// styles
+import './App.css';
+
 // attach app to dom
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
